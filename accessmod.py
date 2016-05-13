237826198865224efa39e3627f3da79a75b0dcc2
Bálint Juhász <jbebe@users.noreply.github.com>
2016-05-13 16:32:54 +0200

    Update accessmod.py

--- accessmod.py
+++ accessmod.py
@@ -1,24 +1,50 @@
 import inspect
 
-def private(function):
-    def oop_violation_error():
-        raise AttributeError(
-            "Private attributes and methods are only " +
-            "accessible from the owner class."
-        )
+def __accessmod_error():
+    raise AttributeError(
+        "Private attributes and methods are only " +
+        "accessible from the owner class."
+    )
+
+def public(method):
     def wrapper(*args, **kwargs):
-        class_instance = inspect.currentframe().f_back.f_locals.get("self", None)
-        if class_instance is None:
-            oop_violation_error()
-        caller_class = class_instance.__class__.__qualname__
-        fn_name = function.__name__
-        qualified_fn_name = function.__qualname__
-        if qualified_fn_name != "{0}.{1}".format(caller_class, fn_name):
-            oop_violation_error()
-        return function(*args, **kwargs)
+        return method(*args, **kwargs)
     return wrapper
 
-def public(function):
+def private(method):
     def wrapper(*args, **kwargs):
-        return function(*args, **kwargs)
+        caller_self = inspect.currentframe().f_back.f_locals.get("self")
+        if caller_self is None:
+            # not called from class
+            __accessmod_error()
+        caller_full_name = caller_self.__class__.__qualname__
+        func_name = method.__name__
+        func_full_name = method.__qualname__
+        caller_full_fn_name = "{0}.{1}".format(caller_full_name, func_name)
+        if func_full_name != caller_full_fn_name:
+            __accessmod_error()
+        return method(*args, **kwargs)
+    return wrapper
+
+def protected(method):
+    def wrapper(*args, **kwargs):
+        def find_class(class_str, child_class):
+            if child_class.__qualname__ == class_str:
+                return True
+            for base in child_class.__bases__:
+                if base.__qualname__ == class_str:
+                    return True
+                if base.__bases__ != (object,):
+                    find_class(class_str, base.__bases__)
+            return False
+        caller_self = inspect.currentframe().f_back.f_locals.get("self")
+        if caller_self is None:
+            __accessmod_error()
+        caller_type = caller_self.__class__
+        func_full_name = method.__qualname__
+        func_class_name_end = func_full_name.rfind('.')
+        func_class_full_name = func_full_name[0:func_class_name_end]
+        if not find_class(func_class_full_name, caller_type):
+            __accessmod_error()
+        return method(*args, **kwargs)
     return wrapper
